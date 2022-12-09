9c10003472adfba7ee47b1714c19a8f81eae6ea7
ecndn <ecndnn@gmail.com>
2022-12-09 15:02:41 +0100

    Update surveys.js
    Rate limit feature added.

--- api/routes/surveys.js
+++ api/routes/surveys.js
@@ -2,34 +2,70 @@ import express from "express";
 import Surveys from "../models/Surveys.js"
 import { createError } from "../utils/error.js";
 import { createSurvey, updateSurvey, deleteSurvey, getSurvey, getSurveys, updateRatings, getPendings, getOpened, getMySurveys } from "../controllers/surveys.js";
-
+import rateLimit from 'express-rate-limit';
 const router = express.Router();
 
+const apiLimitMin = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 20,  
+    message:
+        'You sent too many requests, please try again later.',
+    standardHeaders: true, 
+    legacyHeaders: false, 
+})
+
 //CREATE
-router.post("/", createSurvey);
+router.post("/", apiLimitMin, createSurvey, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
 //UPDATE
-router.put("/:id", updateSurvey);
+router.put("/:id", apiLimitMin, updateSurvey, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
 //UPDATE RATINGS
-router.put("/ratings/:id/:userId", updateRatings)
+router.put("/ratings/:id/:userId", apiLimitMin, updateRatings, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
 //DELETE
-router.delete("/:id", deleteSurvey);
- 
+router.delete("/:id", apiLimitMin, deleteSurvey, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
+
 //GET
-router.get("/:id", getSurvey);
- 
+router.get("/:id", apiLimitMin, getSurvey, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
+
+
 //GET ALL
-router.get("/", getSurveys);
+router.get("/", apiLimitMin, getSurveys, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
 //GET OPEN SURVEYS
-router.get("/opened/:status", getOpened);
+router.get("/opened/:status", apiLimitMin, getOpened, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
 //GET PENDINGS
-router.get("/pending/:status", getPendings);
+router.get("/pending/:status", apiLimitMin, getPendings, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
 // GET MY SURVEYS
-router.get("/getmysurvey/:created", getMySurveys);
-
+router.get("/getmysurvey/:created", apiLimitMin, getMySurveys, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+})
 export default router
