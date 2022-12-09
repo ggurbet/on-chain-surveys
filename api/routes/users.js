8407ff6c03030dbebd2f1a62cdd58771c81a28a8
ecndn <ecndnn@gmail.com>
2022-12-09 15:07:44 +0100

    Update users.js
    Rate limit feature added.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
 import { createError } from "../utils/error.js";
-import { tokenVerify, userVerify, adminVerify } from "../utils/tokenVerify.js";
-
+import { tokenVerify, userVerify, adminVerify } from "../utils/tokenVerify.js"; 
+import rateLimit from 'express-rate-limit';
 const router = express.Router();
+
+const apiLimitMin = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 20,  
+    message:
+        'You sent too many requests, please try again later.',
+    standardHeaders: true, 
+    legacyHeaders: false, 
+})
 
 //AUTHENTICATION CHECK 
 router.get("/authenticationcheck", tokenVerify, (req, res, next) => {
@@ -21,19 +30,30 @@ router.get("/admincheck/:id", adminVerify, (req, res, next) => {
 });
 
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", apiLimitMin, updateUser, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
 
 //DELETE
-router.delete("/:id", deleteUser);
- 
-//GET
-router.get("/:id", getUser);
+router.delete("/:id", apiLimitMin, deleteUser, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
+//GET
+router.get("/:id", apiLimitMin, getUser, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+}); 
 
 //GET ALL
 
-router.get("/", getUsers);
+router.get("/", apiLimitMin, getUsers, (req, res) => {
+    req.apiLimitMin;
+    res.status(429).json();
+});
 
 
 export default router
